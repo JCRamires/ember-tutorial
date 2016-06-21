5f17d86b5fccb17e43aed21b2ff646990e70a28d
Juan Carlos <jcramires851@gmail.com>
2016-06-21 14:10:56 -0300

    filter by city

--- mirage/config.js
+++ mirage/config.js
@@ -1,7 +1,7 @@
 export default function () {
-    this.get('/rentals', function () {
-        return {
-            data: [{
+    this.get('/rentals', function(db, request) {
+        let rentals = [
+            {
                 type: 'rentals',
                 id: 1,
                 attributes: {
@@ -12,7 +12,8 @@ export default function () {
                     bedrooms: 15,
                     image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
                 }
-            }, {
+            },
+            {
                 type: 'rentals',
                 id: 2,
                 attributes: {
@@ -23,7 +24,8 @@ export default function () {
                     bedrooms: 1,
                     image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
                 }
-            }, {
+            },
+            {
                 type: 'rentals',
                 id: 3,
                 attributes: {
@@ -34,7 +36,16 @@ export default function () {
                     bedrooms: 3,
                     image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
                 }
-            }]
+            }
+        ]
+
+        if(request.queryParams.city !== undefined){
+            let filteredRentals = rentals.filter(function(i) {
+                return i.attributes.city.toLowerCase().indexOf(request.queryParams.city.toLowerCase()) !== -1
+            })
+            return {data: filteredRentals}
+        } else {
+            return {data: rentals}
         }
     })
 }
